7e5457ba70e1e8f8e251832434e81f09373dd47a
Rodrigo Baima <rodrigo.baima.10@gmail.com>
2026-05-22 13:40:22 -0300

    Revert "commit com os erros"

--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,3 @@
 from django.db import models
-from usuarios.models import Usuario
 
-class Postagem(models.Model):
-    titulo_postagem = models.CharField(max_length=45, blank=True, null=True)
-    data_hora = models.DateTimeField(auto_now_add=True)
-    conteudo = models.TextField(blank=True, null=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'postagens'
-
-    def __str__(self):
-        return self.titulo_postagem or "Post"
+# Create your models here.

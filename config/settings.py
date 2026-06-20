515ca473dd2ae526bcc4a088e37c93528b0bd82d
Rodrigo Sales Baima Pinto <rodrigo.baima.10@gmail.com>
2026-06-20 08:31:29 -0300

    removi minha senha do meu banco de dados, para não quebrar o banco de dados do projeto

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'root',
-        'PASSWORD': 'admin',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }

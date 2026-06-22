6f27cf48437b34230d7a894fb735cff6b12b9d5a
rsnproj2 <renan.nascimento6@gmail.com>
2026-06-22 10:17:50 -0300

    url do dashboard resolvido, falta um botão.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import painel_admin
 
 urlpatterns = [
-    path('', painel_admin, name='painel_admin'),
+    path('painel-admin/', painel_admin, name='painel_admin'),
 ]

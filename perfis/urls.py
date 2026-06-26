ef4a4110eec14596e1064bd63a6a3f7409e8c748
rsnproj2 <renan.nascimento6@gmail.com>
2026-06-26 10:13:42 -0300

    Novo VENV máquina do SENAC

--- perfis/urls.py
+++ perfis/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views # views nativas de autenticação do Django
 from .views import perfil, virar_autor
 
 # Namespace do app (boa prática)
@@ -8,4 +9,9 @@ urlpatterns = [
     # Rota do perfil do usuário
     path('perfil/', perfil, name='perfil_pessoal'),
     path('perfil/upgrade/', virar_autor, name='virar_autor'), # <-- Nova Rota para Autores
+    
+    # Nova rota chamando a ferramenta de senha do Django
+    path('alterar-senha/', auth_views.PasswordChangeView.as_view(template_name='perfis/alterar_senha.html', # página dentro do app perfis
+            success_url='/conta/perfil/' # Volta para o perfil quando der certo
+            ), name='alterar_senha'),
 ]

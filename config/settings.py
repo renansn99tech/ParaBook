8ae401f6ce87729d61637564902f98c7ad1294f9
Renan <renan.nascimento6@gmail.com>
2026-07-12 23:28:30 -0300

    removendo pssw BD

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": config("DB_NAME", default="mydb"),
         "USER": config("DB_USER", default="root"),
-        "PASSWORD": config("DB_PASSWORD", default="Renann13."),
+        "PASSWORD": config("DB_PASSWORD", default=""),
         "HOST": config("DB_HOST", default="127.0.0.1"),
         "PORT": config("DB_PORT", default="3306"),
         "OPTIONS": {"ssl": {"ssl_mode": "REQUIRED"}},

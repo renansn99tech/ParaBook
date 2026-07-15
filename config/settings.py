2dc95f0eebdbfd6242ef07b2c2768ec44a703409
Rodrigo Sales Baima Pinto <rodrigo.baima.10@gmail.com>
2026-07-14 21:46:16 -0300

    correção da estrutura do django e auditoria

--- config/settings.py
+++ config/settings.py
@@ -15,8 +15,6 @@ from decouple import config
 import dj_database_url 
 import os
 
-import usuarios
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1z+q%(deeme==owa&2+iq5jmk)3r*4-ma8+oea!a)p9$1))u=f'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-substitua-isso-em-producao')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  config('DEBUG', default=True, cast=bool) 

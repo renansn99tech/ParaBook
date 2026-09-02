fbe3bb0b2f258871a10c1b7cb9e8f5ce6c324727
Rodrigo Sales Baima Pinto <rodrigo.baima.10@gmail.com>
2026-09-02 08:18:12 -0300

    password hashing in seed_admin command

--- payroll/management/commands/seed_admin.py
+++ payroll/management/commands/seed_admin.py
@@ -5,19 +5,27 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Command(BaseCommand):
-    help = 'Cria o superusuario inicial automaticamente caso ele nao exista.'
+    help = 'Garante a criação ou atualização do superusuário inicial.'
 
     def handle(self, *args, **options):
         username = os.getenv('SEED_ADMIN_USERNAME', 'renan-adm')
         email = os.getenv('SEED_ADMIN_EMAIL', 'renan.nascimento6@gmail.com')
         password = os.getenv('SEED_ADMIN_PASSWORD', 'parabook123')
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username, 
-                email=email, 
-                password=password
-            )
-            self.stdout.write(self.style.SUCCESS(f'Superusuario "{username}" criado com sucesso!'))
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={'email': email, 'is_staff': True, 'is_superuser': True}
+        )
+
+        # Força a atualização do hash da senha, e-mail e privilégios
+        user.email = email
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.set_password(password)  # Criptografa a senha corretamente
+        user.save()
+
+        if created:
+            self.stdout.write(self.style.SUCCESS(f'Superusuário "{username}" criado com sucesso!'))
         else:
-            self.stdout.write(self.style.WARNING(f'Superusuario "{username}" ja existe no banco de dados.'))
+            self.stdout.write(self.style.SUCCESS(f'Senha e permissões do superusuário "{username}" foram atualizadas!'))
